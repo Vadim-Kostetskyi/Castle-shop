0b7e822bd4143af630d4b2e437d6f1580463d81e
Vadim-Kostetskyi <rasty2308@gmail.com>
2025-01-11 21:58:14 +0200

    xreate progect

---
+++ src/pages/App.tsx
@@ -0,0 +1,11 @@
+import Header from "components/core/containers/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/translations/i18n.ts
@@ -0,0 +1,27 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import translationEn from "./locales/en/translation.json";
+import translationUa from "./locales/ua/translation.json";
+import validationEn from "./locales/en/validation.json";
+import validationUa from "./locales/ua/validation.json";
+
+const resources = {
+  en: {
+    translation: translationEn,
+    validation: validationEn,
+  },
+  ua: {
+    translation: translationUa,
+    validation: validationUa,
+  },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "ua",
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
